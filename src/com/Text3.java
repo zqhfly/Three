126aa066357ac0f3e0dc15efd89f7723291ade8c
zqhfly <1762029177@qq.com>
2014-06-19 15:18:28 +0800

    update Three

---
+++ src/com/Text3.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Text3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello!");
+
+		System.out.println("hello!");
+
+		System.out.println("hello!");
+	}
+
+}
